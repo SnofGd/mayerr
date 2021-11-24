5c9ff76faced68b3bdf250dd7fae5ed63c434406
SnofGd <egor.login90@gmail.com>
2021-11-24 19:21:45 +0500

    first

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _2323
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("******************");
+            Console.WriteLine("***Тест Пратики***");
+            int i = 5;
+
+            Console.WriteLine("******************");
+            // Основка
+            int b = 3;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine();
+            int c = i + b;
+            Console.WriteLine("Проверчока практики");
+            // там будет ожидание, надо бут ентер нажать
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
